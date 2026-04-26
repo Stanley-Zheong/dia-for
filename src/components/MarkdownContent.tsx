0f6ff28995763b8d12690cb62f9cae1f97502dc3
stanley-zheong <heszhang@163.com>
2026-04-26 08:51:13 +0800

    modity the nested markdown

--- src/components/MarkdownContent.tsx
+++ src/components/MarkdownContent.tsx
@@ -2,15 +2,19 @@ import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
 
+import { normalizeMarkdownExampleBlocks } from "@/lib/markdown-example";
+
 type MarkdownContentProps = {
   content: string;
 };
 
 export function MarkdownContent({ content }: MarkdownContentProps) {
+  const normalizedContent = normalizeMarkdownExampleBlocks(content);
+
   return (
     <div className="markdown-body">
       <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
-        {content}
+        {normalizedContent}
       </ReactMarkdown>
     </div>
   );
